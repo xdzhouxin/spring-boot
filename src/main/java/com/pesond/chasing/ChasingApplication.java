0ed940ae4a7b58671cb49d89a0d537626de2a56b
zhouxin <xdzhouxin@163.com>
2020-07-19 00:04:39 +0800

    INIT

---
+++ src/main/java/com/pesond/chasing/ChasingApplication.java
@@ -0,0 +1,13 @@
+package com.pesond.chasing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChasingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChasingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pesond/chasing/ChasingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pesond.chasing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChasingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
